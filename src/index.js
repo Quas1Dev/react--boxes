76239db29b089203a1d4bafd476c92a95df81cc1
Fernando Bonfim <fernando.j.bonfim@gmail.com>
2025-01-19 05:58:00 -0300

    Added semi-colons; Corrected app to App.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./app"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-let root = ReactDOM.createRoot(document.getElementById("root"))
+let root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <App />
